1f8324051aa0177d5308ff8683afde0a14c50a2d
Daniel <danielmata2@my.unt.edu>
2020-09-17 14:18:16 -0500

    removed key AND updated gps list return

--- google_api.py
+++ google_api.py
@@ -7,10 +7,10 @@ class Coordinates:
         # Initial info
         self.origin = origin.replace(' ' ,'+')
         self.destination = destination.replace(' ','+')
-        self.key = "API_Key"
+        self.key = "API KEY"
         self.directions_url = "https://maps.googleapis.com/maps/api/directions/json?"
         self.map_url  = "https://maps.googleapis.com/maps/api/staticmap?"
-        self.gps_coord = []
+        self.gps_coord_pairs = []
     
     def return_coordinates(self):
         # Directions API
@@ -18,12 +18,21 @@ class Coordinates:
         request_1 = self.directions_url + coordinates
         response_1 = requests.get(request_1)
         directions = response_1.json()
-    
+        
+        self.gps_coord = []
+        
         # Get coordinates list
         for i in directions['routes'][0]['legs'][0]['steps']:
             self.gps_coord.append(i['start_location'])
             
-        return self.gps_coord
+        self.gps_coord_pairs = []
+        
+        for dictionary in self.gps_coord:
+            pair = list(dictionary.values())
+            self.gps_coord_pairs.append(pair)
+                
+        return self.gps_coord_pairs
+                
     
     def return_image(self):
         gps_string = str(self.gps_coord)
