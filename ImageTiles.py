c030b38c13590b4795826cea6c442c048d8d7cdb
zachary-obrien <34065126+zachary-obrien@users.noreply.github.com>
2020-09-21 22:30:30 -0500

    Update ImageTiles.py

--- ImageTiles.py
+++ ImageTiles.py
@@ -4,9 +4,25 @@ import json
 import xmltodict
 from pascal_voc_writer import Writer
 
-class image_cropper:
 
+class image_cropper:
+    # This class provides the ability to separate the images into tiles of height x width size
+    # The leftover edges are dropped and it returns a dictionary of crop_boxes[image_tile_name] = (bounds for section of image)
+    # This box is given so that it can be used to separate the bounding boxes of corresponding VOC files (or other labeled training data)
     def __crop(self,infile,height,width):
+        '''Internal class that crops a specific image into tiles. 
+        Parameters
+        ----------
+        infile : str
+            the file path and name of an image 
+        height : int
+            the desired tile height
+        width : int
+            the desired tile width
+        Returns
+        -------
+        The image tiles via generator
+        '''
         with Image.open(infile) as im:
             imgwidth, imgheight = im.size
             for i in range(imgheight//height):
@@ -15,6 +31,24 @@ class image_cropper:
                     yield (box, im.crop(box))
 
     def crop(self,infile,outfolder,height,width,start_num):
+        '''Wrapper for the internal crop function that handles the 
+             management of file names as well as saving the new images. 
+        Parameters
+        ----------
+        infile : str
+            the file path and name of an image 
+        outfolder : str
+            the folder that the tile png's should be written to 
+        height : int
+            the desired tile height
+        width : int
+            the desired tile width
+        start_num : int
+            the number that will be used to start file numbering
+        Returns
+        -------
+        Dictionary of crop_boxes[image_tile_name] = (bounds for section of image)
+        '''
         crop_boxes = dict()
         for k,(box,piece) in enumerate(self.__crop(infile,height,width),start_num):
             img=Image.new('RGB', (height,width), 255)
@@ -27,7 +61,15 @@ class image_cropper:
         return crop_boxes
 
 class voc_tiler: 
+    # This class takes a given VOC/Pascal annotated file and divides itself and associated image up into corresponding tiles
+    # e.g. a 1920 x 1080 image broken could be broken into a 3 by 2 grid of 512 x 512 images (The leftover area is discarded) 
     def __init__(self):
+        ''' json_body will store the incoming voc file for easy parsing
+            image_tile_boxes is a dictionary of image_tile_name => (bounds for section of image)
+            image_data_points is a dictionary of image_tile_name and the associated bounding boxes found in
+              the original data set
+            new_voc_files is a dictionary of voc_file_name -> voc data to be written to file
+        '''
         self.json_body = None
         #self.image_tile_boxes[<image>] = <box encompased>
         self.image_tile_boxes = dict()
@@ -35,16 +77,30 @@ class voc_tiler:
         self.new_voc_files = dict()
 
     def __crop_image(self,infile,outfolder,height,width,start_number):
+        '''Wrapper for the image_cropper crop function
+            See image_cropper.crop() for parameter details
+        '''
         ic = image_cropper()
         box_dict = ic.crop(infile,outfolder,height,width,start_num)
         return box_dict
 
     def __parse_voc_file(self,infile):
+        '''Reads a voc file into the json body for easy parsing
+        '''
         with open(infile) as xml_file:
             data_dict = xmltodict.parse(xml_file.read())
             self.json_body = json.dumps(data_dict)
 
     def split_voc_and_images(self,xmlfile,outfolder,height,width,start_num):
+        '''Overall logic funtion
+          1. Parses Voc file into json body
+          2. crop image to given measurements and retrieve dictionary of image name / bounding boxes
+          3. loop through bounding boxes and find associated data points 
+            3b. If bounding box overlaps tiles, shorten the box to fit in its corresponding tile
+          4. Create the object list that represents each bounding box for a given image
+          5. Create object that represents a single one of the VOC tiles and place it in self.new_voc_files
+
+        '''
         self.__parse_voc_file(xmlfile)
         self.image_tile_boxes = self.__crop_image(self.json_body["annotation"]["path"],outfolder,height,width,start_num)
         min_points = 0
@@ -80,6 +136,8 @@ class voc_tiler:
                         "bndbox":{"xmin":x_min, "ymin":y_min, "xmax":x_max, "ymax":y_max}}
                     self.image_data_points[image_file]["object"].append(data_object)
                     min_points += 1
+
+            #Check if there's at least 1 bounding box in the tile before creating a new file
             if min_points:
                 folder = outfolder
                 filename = image_file.split("\\")[-1]
@@ -94,6 +152,8 @@ class voc_tiler:
                     "object":self.image_data_points[image_file]["object"]}
 
     def write_new_vocs(self):
+        # This is where the objects listed in self.new_voc_files actually get written to their 
+        # corresponding XML files
         for new_file in self.new_voc_files.keys():
             with open(new_file, "w") as out_file: 
                 print("Write a voc file")
@@ -104,13 +164,12 @@ class voc_tiler:
 
 
 if __name__=='__main__':
-    #TODO: Loop through xml files
+    #TODO Loop through xmls
     infile="<something.xml>"
-    outfolder = "<some output folder> "
+    outfolder = "<some output folder>"
     height=512
     width=512
     start_num=0
-    
     ic = image_cropper()
     ic.crop(infile,outfolder,height,width,start_num)
     
